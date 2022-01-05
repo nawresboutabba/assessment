7b65956965c87799cf50ccbf8d96d2329dea37b8
Nawres <nawres.boutabba@esprit.tn>
2022-01-05 10:34:47 +0100

    fixing

--- src/Utils/Urls.js
+++ src/Utils/Urls.js
@@ -1,6 +1,4 @@
 export const baseURL = "http://js-assessment-backend.herokuapp.com/users";
-
-
 export const headers = { "Content-Type": "application/json" };
 export const statuses = { ACTIVE: "active", LOCKED: "locked" };
 export const formTitles = { EDIT: "Edit User", ADD: "Add User" };
